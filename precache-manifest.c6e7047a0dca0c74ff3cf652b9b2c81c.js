a94cb1cd4ae61d419f628e24c385fd36fa362780
weelliem <williamhong@live.com.au>
2020-08-14 02:55:11 +1000

    Updates

---
+++ precache-manifest.c6e7047a0dca0c74ff3cf652b9b2c81c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4427ce021b7f8c209def7333b24ad3c2",
+    "url": "/React-Website/index.html"
+  },
+  {
+    "revision": "ebb670db794e5f73e702",
+    "url": "/React-Website/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "93193bd67bcd7b7421e1",
+    "url": "/React-Website/static/css/main.f748b7d5.chunk.css"
+  },
+  {
+    "revision": "ebb670db794e5f73e702",
+    "url": "/React-Website/static/js/2.23da1976.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/React-Website/static/js/2.23da1976.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93193bd67bcd7b7421e1",
+    "url": "/React-Website/static/js/main.bb82f0e9.chunk.js"
+  },
+  {
+    "revision": "d8486d080c8f13e5975d",
+    "url": "/React-Website/static/js/runtime-main.76a26adf.js"
+  },
+  {
+    "revision": "9e82c2fe00ff4fd6fcf7204c1dfe6d43",
+    "url": "/React-Website/static/media/Covid.9e82c2fe.jpg"
+  },
+  {
+    "revision": "d94b9d4c180d1c07c66ae4f78cbdff09",
+    "url": "/React-Website/static/media/Snap1.d94b9d4c.jpg"
+  },
+  {
+    "revision": "5693dd1410a24878b26ef42688613a06",
+    "url": "/React-Website/static/media/UAsk.5693dd14.jpeg"
+  },
+  {
+    "revision": "a137b9443fd03b289c3b387b6c1f7f37",
+    "url": "/React-Website/static/media/backgroundGrey.a137b944.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a69b76f954c1bdb27a2c040aecb05cb2",
+    "url": "/React-Website/static/media/vector.a69b76f9.png"
+  }
+]);
